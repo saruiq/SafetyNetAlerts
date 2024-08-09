5123ad6959b477adf8e57e61790d8a40e6695733
iqbal <iqbal@DESKTOP-PO9KLPQ>
2024-08-09 01:00:25 -0500

    Adding some test cases for controller class

---
+++ src/test/java/com/openclassrooms/safetynetalerts/SafetyNetAlertsControllerTests.java
@@ -0,0 +1,60 @@
+package com.openclassrooms.safetynetalerts;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.openclassrooms.safetynetalerts.controller.SafetyNetAlertsController;
+import com.openclassrooms.safetynetalerts.model.FireStations;
+import com.openclassrooms.safetynetalerts.service.FireStationService;
+import com.openclassrooms.safetynetalerts.service.MedicalRecordService;
+import com.openclassrooms.safetynetalerts.service.PersonService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = SafetyNetAlertsController.class)
+@AutoConfigureMockMvc
+public class SafetyNetAlertsControllerTests {
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private FireStationService fireStationService;
+	
+	@MockBean
+	private MedicalRecordService medicalRecordService;
+	
+	@MockBean
+	private PersonService personService;
+
+    @BeforeEach
+    public void setup () {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void listOfPersonsByFireStationTest() throws Exception {
+        mockMvc.perform(get("/firestation", "1")).andExpect(status().isOk());
+    }
+    
+
+}
