d8996675bac9516acf17b6caa50430796ad2a734
iqbal <iqbal@DESKTOP-PO9KLPQ>
2024-08-15 23:04:09 -0500

    Added jacoco maven plugin and working on test cases

--- src/test/java/com/openclassrooms/safetynetalerts/SafetyNetAlertsControllerTests.java
+++ src/test/java/com/openclassrooms/safetynetalerts/SafetyNetAlertsControllerTests.java
@@ -1,10 +1,12 @@
 package com.openclassrooms.safetynetalerts;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.openclassrooms.safetynetalerts.controller.SafetyNetAlertsController;
+import com.openclassrooms.safetynetalerts.model.PersonsProfile;
 import com.openclassrooms.safetynetalerts.service.FireStationService;
 import com.openclassrooms.safetynetalerts.service.MedicalRecordService;
 import com.openclassrooms.safetynetalerts.service.PersonService;
@@ -38,8 +41,9 @@ public class SafetyNetAlertsControllerTests {
 	@MockBean
 	private PersonService personService;
 	
-	@MockBean
-	private static JsonParser jsonParser;
+	@Mock
+    PersonsProfile personsProfile = new PersonsProfile();
+	
 
     @BeforeEach
     public void setup () {
@@ -50,6 +54,11 @@ public class SafetyNetAlertsControllerTests {
     @Test
     void listOfPersonsByFireStationTest() throws Exception {
         mockMvc.perform(get("/firestation?stationNumber=1")).andExpect(status().isOk());
+    }
+    
+    @Test
+    void createPersonsTest() throws Exception {
+        mockMvc.perform(post("/person")).andExpect(status().isOk());
     }
     
 
